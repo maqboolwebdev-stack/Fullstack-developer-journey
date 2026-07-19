645693056267e593c8d84c798e4986197901074d
waqas <maqbool.webdev@gmail.com>
2026-07-19 22:17:44 +0500

    Solved Inverted Right angle triangle

--- JavaScript-TOP/21-days-challange-js/day-03/star-patterns.js
+++ JavaScript-TOP/21-days-challange-js/day-03/star-patterns.js
@@ -2,11 +2,22 @@
 
 let rows = 5;
 
-for(let i = 1; i <= rows; i++) {
-    let star = '';
-    for(let j = 1; j <= i; j++) {
+for (let i = 1; i <= rows; i++) {
+  let star = '';
+  for (let j = 1; j <= i; j++) {
+    star += '*';
+  }
+  console.log(star);
+}
 
-        star += '*';
-    }
-    console.log(star);
+// Q2 Inverted Right-Angled star triangle
+
+// let rows2 = 5;
+
+for (let i = rows; i >= 1; i--) {
+  let star = '';
+  for (let j = i; j >= 1; j--) {
+    star += '*';
+  }
+  console.log(star);
 }
