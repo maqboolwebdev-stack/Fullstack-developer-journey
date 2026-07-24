026b5a65917a3a4ff14a64f559e2d405dc9bd1e3
waqas <maqbool.webdev@gmail.com>
2026-07-24 15:33:39 +0500

    overview on npm

---
+++ JavaScript-TOP/npm/script.js
@@ -0,0 +1,9 @@
+console.log('Hello! form JavaScript');
+
+console.log(moment().startOf('day').fromNow());
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+console.log(moment().format('dddd')); 
+console.log(moment().format("MMM Do YY"));
+console.log(moment().format('YYYY [escaped] YYYY')); 
+console.log(moment().format());
