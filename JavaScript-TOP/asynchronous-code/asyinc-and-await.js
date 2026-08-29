06fc376694621648a3d00f5d42e2f34d35ae61bc
Waqas Maqbool <maqbool.webdev@gmail.com>
2026-08-29 17:52:23 +0500

    Learn about async await

--- JavaScript-TOP/asynchronous-code/asyinc-and-await.js
+++ JavaScript-TOP/asynchronous-code/asyinc-and-await.js
@@ -45,10 +45,10 @@ const yourAsyncFunction = async () => {
   return result;
 }
 
-anArray.forEach(async item => {
+// anArray.forEach(async item => {
   // do something asynchronously for each item in 'anArray'
   // one could also use .map here to return an array of promises to use with 'Promise.all()'
-});
+// });
 
 
 server.getPeople().then(async people => {
@@ -58,9 +58,9 @@ server.getPeople().then(async people => {
 });
 
 
-asyncFunctionCall().catch(err => {
-  console.error(err)
-});
+// asyncFunctionCall().catch(err => {
+//   console.error(err)
+// });
 
 
 async function getPersonsInfo(name) {
@@ -72,3 +72,25 @@ async function getPersonsInfo(name) {
     // Handle the error any way you'd like
   }
 }
+
+
+function doubleAfter2Seconds(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x * 2);
+    }, 2000);
+  });
+}
+
+async function addAsync(x) {
+  const a = await doubleAfter2Seconds(10);
+  const b = await doubleAfter2Seconds(20);
+  const c = await doubleAfter2Seconds(30);
+  return x + a + b + c;
+}
+
+
+addAsync(99).then((sum) => {
+  console.log(sum);
+});
+
