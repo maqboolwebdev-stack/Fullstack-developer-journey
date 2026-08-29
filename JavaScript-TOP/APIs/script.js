110bb56d7794fe04b6d0ec0c9528419987f47e7a
Waqas Maqbool <maqbool.webdev@gmail.com>
2026-08-29 11:32:21 +0500

    Refactor code with async and await try and catch

--- JavaScript-TOP/APIs/script.js
+++ JavaScript-TOP/APIs/script.js
@@ -1,48 +1,53 @@
-const img = document.querySelector("img");
-      const btn = document.querySelector("button");
-      const searchBox = document.querySelector("input");
+const img = document.querySelector('img');
+const btn = document.querySelector('button');
+const searchBox = document.querySelector('input');
 
-      const gifContainer = document.querySelector(".gif-container");
-      const searchBtn = document.querySelector(".search-btn");
+const gifContainer = document.querySelector('.gif-container');
+const searchBtn = document.querySelector('.search-btn');
 
-      btn.addEventListener("click", function () {
-        fetch(
-          "https://api.giphy.com/v1/gifs/translate?api_key=3pDrfCFw6TxmqbAwqlKszOgrEP1hGHdN&s=cats",
-        )
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (response) {
-            img.src = response.data.images.original.url;
-          });
-      });
+async function getCats() {
+  try {
+    const response = await fetch(
+      'https://api.giphy.com/v1/gifs/translate?api_key=3pDrfCFw6TxmqbAwqlKszOgrEP1hGHdN&s=cats',
+    );
+    const catData = await response.json();
+    img.src = catData.data.images.original.url;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-      const apiKey = "3pDrfCFw6TxmqbAwqlKszOgrEP1hGHdN";
+btn.addEventListener('click', getCats);
 
-      searchBtn.addEventListener("click", function () {
-        const query = searchBox.value.trim();
+const apiKey = '3pDrfCFw6TxmqbAwqlKszOgrEP1hGHdN';
 
-        if (!query) {
-          alert("Please enter a search term!");
-          return;
-        }
+async function getCustomMeme() {
+  const query = searchBox.value.trim();
 
-        const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(query)}&limit=5`;
+  if (!query) {
+    alert('Please enter a search term!');
+    return;
+  }
 
-        fetch(url)
-          .then((response) => response.json())
-          .then((content) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(query)}&limit=5`;
 
-            gifContainer.innerHTML = "";
+  try {
+    const response = await fetch(url);
+    const content = await response.json();
 
-            content.data.forEach((gif) => {
-              const img = document.createElement("img");
-              img.src = gif.images.fixed_height.url;
-              img.alt = gif.title;
-              gifContainer.appendChild(img);
-            });
-          })
-          .catch((err) => {
-            console.error("Error fetching data from GIPHY:", err);
-          });
-      });
+    gifContainer.innerHTML = '';
+
+    content.data.forEach((gif) => {
+      const img = document.createElement('img');
+
+      img.src = gif.images.fixed_height.url;
+      img.alt = gif.title;
+
+      gifContainer.appendChild(img);
+    });
+  } catch (err) {
+    console.error('Error fetching data from GIPHY:', err);
+  }
+}
+
+searchBtn.addEventListener('click', getCustomMeme);
